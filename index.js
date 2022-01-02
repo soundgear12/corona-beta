26c779a2750646d79c9f09dd5ba6e6624174903d
Antonio <antonio.rodriguez024@gmail.com>
2022-01-01 23:01:32 -0600

    create a CSV parser

---
+++ index.js
@@ -0,0 +1,32 @@
+const Papa = require('papaparse')
+const fs =  require('fs')
+
+const covidFilePath = './csv/covid.csv'
+const covidFile = fs.readFileSync(covidFilePath, "utf8")
+
+const covidRows = {}
+Papa.parse(covidFile, {
+    header: true,
+    skipEmptyLines: true,
+    complete: function(results) {
+        covidRows.data = results.data
+        covidRows.errors = results.errors
+        covidRows.meta = results.meta
+    }
+})
+
+const covidArray = covidRows.data.map(row => {
+    const { Region, date, Deaths, Recoveries, Active, DeathRateCases } = row
+    
+    return { Region, date, Deaths, Recoveries, Active, DeathRateCases  }
+})
+
+const covidData = Papa.unparse(covidArray)
+createFile("./csv/covidTable.csv", covidData, "Covid table successfully saved!")
+
+function createFile(filePath, data, msg) {
+    fs.writeFile(filePath, data, err => {
+        if (err) throw err;
+        console.log(msg)
+    })
+}
